102016d8d3e69d2a8c9b2c154bb0f1d803a10056
roshan pujara <pujararoshan984@gmail.com>
2023-07-02 20:49:22 +0545

    jwt token added in cookie

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,7 +1,6 @@
 import express,{Request,Response} from "express";
 import userRouter from "./user.route"
 import passport from "../middlewares/passport.middleware";
-
 import jwt from 'jsonwebtoken'
 
 const router = express.Router();
@@ -14,23 +13,23 @@ router.get('/login/failed',(req:Request,res:Response)=>{
     })
 }) 
 
-router.get('/auth/google/callback',passport.authenticate('google',{
-    successRedirect: process.env.CLIENT_URL + '/dashboard',
-    failureRedirect: '/login/failed'
-}))
+// router.get('/auth/google/callback',passport.authenticate('google',{
+//     successRedirect: process.env.CLIENT_URL + '/dashboard',
+//     failureRedirect: '/login/failed'
+// }))
 
 
-// router.get('/auth/google/callback', (req, res, next) => {
-//     passport.authenticate('google', (err: any, user: any) => {
-//         if (err) {
-//             return next(err);
-//         }
-//         const secretKey: string = process.env.JWT_SECRET_KEY ?? '';
-//         const token = jwt.sign(user["_json"].email, secretKey)
-//         res.cookie('jwtToken', token);
-//         res.redirect(`${process.env.CLIENT_URL}/dashboard`);
-//     })(req, res, next);
-// });
-// router.use('/user',userRouter);
+router.get('/auth/google/callback', (req, res, next) => {
+    passport.authenticate('google', (err: any, user: any) => {
+        if (err) {
+            return next(err);
+        }
+        const secretKey: string = process.env.SECRET_KEY ?? '';
+        const token = jwt.sign(user["_json"].email, secretKey)
+        res.cookie('jwtToken', token);
+        res.redirect(`${process.env.CLIENT_URL}/dashboard`);
+    })(req, res, next);
+});
+router.use('/user',userRouter);
 
 export default router
